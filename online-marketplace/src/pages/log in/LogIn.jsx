9c82b9d2526d2f9c228701cd7a664455ee33a644
Ashley Clark <ashleyclarkindy@gmail.com>
2023-10-20 16:16:16 -0400

    created log in page

---
+++ online-marketplace/src/pages/log in/LogIn.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import {Link} from 'react-router-dom'
+
+import './login.css'
+
+const LogIn = () => {
+  return (
+    <Container id='log-in' className='text-center border'>
+      <h2>Welcome to Webstore!</h2>
+      <Form.Control className='input-form' type='text' placeholder='Username' />
+      <Form.Control className='input-form' type='password' placeholder='Password' />
+      <Button id='logIn-btn' variant='primary' type='submit'>Log In</Button>
+      <Nav>
+        <Link id='signUp-btn' to="signup">Don't have an account?</Link>
+      </Nav>
+      {/* <Button id='signIn-btn' variant='secondary'>Sign Up</Button> */}
+    </Container>
+  );
+}
+
+export default LogIn
