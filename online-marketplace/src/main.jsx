5647feb45a92744a020949d5decc73114e2d3b6e
Ashley Clark <ashleyclarkindy@gmail.com>
2023-10-20 16:16:57 -0400

    created initial reouter

--- online-marketplace/src/main.jsx
+++ online-marketplace/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LogIn from './pages/log in/LogIn';
+import SignUp from './pages/sign up/SignUp';
+
+const router = createBrowserRouter([
+  { path:"/",
+    element: <LogIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
