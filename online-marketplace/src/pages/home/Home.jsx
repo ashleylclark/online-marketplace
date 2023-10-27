f53ad0b1270c42dab60d21b8b3914fce8718e439
Ashley Clark <ashleyclarkindy@gmail.com>
2023-10-27 16:02:17 -0400

    removed button

--- online-marketplace/src/pages/home/Home.jsx
+++ online-marketplace/src/pages/home/Home.jsx
@@ -14,16 +14,6 @@ const ShopName = () => {
   )
 }
 
-const Cart = () => {
-  return (
-    <Link id='cart'>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-      </svg>
-    </Link>
-  )
-}
-
 const CustomerHome = () => {
   return (
     <div id='cust-btns'>
@@ -61,7 +51,6 @@ const Home = () => {
             <Navbar.Text>
               Signed in as: <Link>Name</Link>
             </Navbar.Text>
-            {selected === 1 && Cart()}
           </Nav>
       </Navbar>
       {/* Buttons to show seller vs buyer for dev */}
